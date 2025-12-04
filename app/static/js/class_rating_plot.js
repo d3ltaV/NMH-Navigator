8d741bb047fa60eb3596c817e0b7768a33ae3778
Grace Huang <ghuang26@nmhschool.org>
2025-12-03 19:42:30 -0500

    Add click event to display dot on grid
    updated with the specific functions of placing the dot on

--- app/static/js/class_rating_plot.js
+++ app/static/js/class_rating_plot.js
@@ -2,4 +2,17 @@ document.addEventListener(“DOMContentLoaded”, ()-> {
 	const grid = document.getElementById(“grid”);
 	const dot = document.getElementById(“dot”); 
 	if (!grid || !dot) return; 
-	dot.style.display = “none”; 
+	dot.style.display = “none”;
+	grid.addEventListener(“click”, function (event){
+		const box = grid.getBoundingClientRect(); 
+		const x = (event.clientX - box.left) / box.width; 
+		const y = (event.clientY - box.top) / box.height
+		const leftPercent = x*100
+		const topPercent = y*100
+		dot.style.display = “block”
+		dot.style.left = leftPercent + ”%”
+		dot.style.top = topPercent + “%”;
+	});
+});
+
+
